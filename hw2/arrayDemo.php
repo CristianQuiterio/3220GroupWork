fc301114667f6614e941ec1c2cb36eaeeb5aef44
CristianQuiterio <47873089+CristianQuiterio@users.noreply.github.com>
2023-09-07 17:25:43 -0400

    Basic Stuff

--- hw2/arrayDemo.php
+++ hw2/arrayDemo.php
@@ -1,1 +1,18 @@
-
+<html>
+<head>
+<title>Random Array Maker with Statistics</title>
+<link rel ="stylesheet" type="text/css" href="sample.css" >
+</head>
+<body>
+<h1>Random Array Maker with Statistics</h1>
+<?php
+$numRows = $_POST['numRows'];
+$numCols = $_POST['numCols'];
+$minValue = $_POST['minValue'];
+$maxValue = $_POST['maxValue'];
+$exampleInteraction = $minValue * $maxValue;
+print("<p>This page is under development.</p>");
+print("<p>You entered $numRows, $numCols, $minValue, and $maxValue</p>");
+?>
+</body>
+</html>
