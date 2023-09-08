da203d79b79553ea91ba2dbf47ce068ddc3686b9
Caleb-cvw <143757071+Caleb-cvw@users.noreply.github.com>
2023-09-08 09:05:53 -0400

    Updated arrayDemo.php to include table 1 and data processing into table 2

--- hw2/arrayDemo.php
+++ hw2/arrayDemo.php
@@ -5,14 +5,55 @@
 </head>
 <body>
 <h1>Random Array Maker with Statistics</h1>
+<a href = "arrayDemo.html"> Back </a>
 <?php
 $numRows = $_POST['numRows'];
 $numCols = $_POST['numCols'];
 $minValue = $_POST['minValue'];
 $maxValue = $_POST['maxValue'];
-$exampleInteraction = $minValue * $maxValue;
-print("<p>This page is under development.</p>");
-print("<p>You entered $numRows, $numCols, $minValue, and $maxValue</p>");
+//$exampleInteraction = $minValue * $maxValue;
+//print("<p>This page is under development.</p>");
+//print("<p>You entered $numRows, $numCols, $minValue, and $maxValue</p>");
+
+//Array
+$arrayData = [];
+//Filling array with random values
+for ($i = 0; $i < $numRows; $i++){
+  $arrayData = [$i];
+  for ($j = 0; $j < $numCols; $j++) {
+    $arrayData[$i][$j] = rand($minValue, $maxValue);
+  }
+}
+
+//Printing
+print "<h2> Inputed Data</h2>";
+print "<table border = '.5'>";
+foreach ($arrayData as $numRow){
+  print "<tr>";
+  foreach ($numRow as $num){
+    print "<td>" . number_format($num, 3) . "</td>";
+  }
+  print "</tr>";
+}
+print "<table>";
+
+//Process the data and print in 2nd table
+print "<h2>Row Stats </h2>";
+print "<table border = '.5'>";
+Print "<tr><th>Row</th><th>Sum</th><th>Average</th><th>Standard Deviation</th></tr>";
+foreach ($arrayData as $i => $numRows){
+  $sum = array_sum($numRows);
+  $average = $sum / $numCols;
+  $standardDev = 0;
+
+  foreach ($numRow as $value) {
+    $standardDev += pow($value - $average, 2);
+  }
+  $standardDev = sqrt($standardDev / $numCols);
+  
+
+}
+
 ?>
 </body>
 </html>
