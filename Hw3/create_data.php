bc52cd89d2acdbfc51222af22eb747932588b396
CristianQuiterio <47873089+CristianQuiterio@users.noreply.github.com>
2023-09-15 13:39:33 -0400

    Updated Read-In files to read in any amount of lines/changed naming convention to match teacher requirements

---
+++ Hw3/create_data.php
@@ -0,0 +1,130 @@
+<html>
+	<?php
+		//First Name
+		//Set up array to hold info and variables for read-in loop
+		$arrayFirst=[];
+		$numLines = 0;
+		
+		//Open/Close the file and get each line read in
+		$firstNames=fopen("first_names.csv","r");
+		while(!feof($firstNames)){
+			$candidateLine = fgets($firstNames);
+			if($candidateLine != ""){
+				$firstNameLine[$numLines] = $candidateLine;
+				$numLines++;
+			}
+		}				
+		fclose($firstNames);
+		
+		//Separate each line into individual names and add them to the name list
+		$nameNumber = 0;
+		foreach($firstNameLine as $nameLine){
+			$nameArray = explode(",", $nameLine);
+			foreach($nameArray as $name){
+				$arrayFirst[$nameNumber] = $name;
+				$nameNumber++;
+			}
+		}
+		
+		//Show that the names correctly separated - deleted later, just showing it works
+		$check=0;
+		foreach($arrayFirst as $name){
+			print($check.$name."<br>");
+			$check++;
+		}
+		
+		//Last Name
+		//Set up array to hold info and variables for read-in loop
+		$arrayLast=[];
+		$numLines = 0;
+		//Open/Close the file and get each line read in
+		$lastNames=fopen("last_names.txt","r");
+		while(!feof($lastNames)){		
+			$candidateLine = fgets($lastNames);
+			if($candidateLine != ""){
+				$arrayLast[$numLines] = $candidateLine;
+				$numLines++;
+			}
+		}				
+		fclose($lastNames);
+		
+		//Already separated in the list		
+		//Show that the names correctly separated - deleted later, just showing it works
+		$check = 0;
+		foreach($arrayLast as $name){
+			
+			print($check.$name."<br>");
+			$check++;
+		}
+		print("<p>$arrayFirst[0]</p>");
+		print("<p>$arrayLast[2]</p>");
+
+		//Street Name
+		//Set up array to hold info and variables for read-in loop
+		$arrayStreet=[];
+		$numLines = 0;
+		//Open/Close the file and get each line read in
+		$streetNames=fopen("street_names.txt","r");
+		while(!feof($streetNames)){
+			$candidateLine = fgets($streetNames);
+			if($candidateLine != ""){
+				$streetNameLine[$numLines] = $candidateLine;
+				$numLines++;
+			}
+		}				
+		fclose($streetNames);
+		
+		//Separate each line into individual names and add them to the name list
+		$nameNumber = 0;
+		foreach($streetNameLine as $nameLine){
+			$nameArray = explode(":", $nameLine);
+			foreach($nameArray as $name){
+				$arrayStreet[$nameNumber] = $name;
+				$nameNumber++;
+			}
+		}
+		
+		//Show that the names correctly separated - deleted later, just showing it works
+		$check=0;
+		foreach($arrayStreet as $name){
+			print($check.$name."<br>");
+			$check++;
+		}
+		print("<p>$arrayStreet[0]</p>");
+
+		//Street Type
+		//Set up array to hold info and variables for read-in loop
+		$arrayType=[];
+		$numLines = 0;
+		//Open/Close the file and get each line read in
+		$streetType=fopen("street_types.txt","r");
+		while(!feof($streetType)){
+			$candidateLine = fgets($streetType);
+			if($candidateLine != ""){
+				$streetTypeLine[$numLines] = $candidateLine;
+				$numLines++;
+			}
+		}				
+		fclose($streetType);
+		
+		//Separate each line into individual names and add them to the name list
+		$nameNumber = 0;
+		foreach($streetTypeLine as $nameLine){
+			$nameArray = explode("..;", $nameLine);
+			foreach($nameArray as $name){
+				$arrayType[$nameNumber] = $name;
+				$nameNumber++;
+			}
+		}
+		
+		//Show that the names correctly separated - deleted later, just showing it works
+		$check=0;
+		foreach($arrayType as $name){
+			print($check.$name."<br>");
+			$check++;
+		}
+		print("<p>$arrayType[0]</p>");
+		
+		
+	?>
+</html>
