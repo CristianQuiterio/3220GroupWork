98cdb9a65e4a468a8a316d85081b81ab5ec69fd6
Jaytonn-H02 <122837315+Jaytonn-H02@users.noreply.github.com>
2023-10-25 19:23:45 -0400

    Add files via upload
    Just gathered some text files, gonna update more soon

---
+++ hw5/schema.sql
@@ -0,0 +1,67 @@
+drop schema
+Create Schema
+
+Use
+
+CREATE TABLE `address` (
+  `address_id` int(20) NOT NULL AUTO_INCREMENT,
+  `street` varchar(100) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `zip` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`address_id`)
+);
+
+CREATE TABLE `customer` (
+  `customer_id` int(20) NOT NULL auto_increment,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(12) DEFAULT NULL,
+  `address_id` int(20) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`),
+  FOREIGN KEY (`address_id`) REFERENCES `address`(`address_id`)
+);
+
+CREATE TABLE `order` (
+  `customer_id` int(20) NOT NULL auto_increment,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(12) DEFAULT NULL,
+  `address_id` int(20) DEFAULT NULL,
+  PRIMARY KEY (`order_id`),
+  FOREIGN KEY (`customer_id`) REFERENCES `customer`(`customer_id`)
+  FOREIGN KEY (`address_id`) REFERENCES `address`(`address_id`)
+);
+
+CREATE TABLE `product` (
+  `product_id` int(20) NOT NULL auto_increment,
+  `product_name` varchar(100) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `weight` varchar(100) DEFAULT NULL,
+  `base_cost` varchar(12) DEFAULT NULL,
+  PRIMARY KEY (`product_id`),
+);
+
+CREATE TABLE `warehouse` (
+  `warehouse_id` int(20) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `address_id` int(20) DEFAULT NULL,
+  PRIMARY KEY (`warehouse_id`),
+  FOREIGN KEY (`address_id`) REFERENCES `address`(`address_id`)
+);
+
+CREATE TABLE `order_item` (
+  `quantity` int(20) NOT NULL auto_increment,
+  `price` varchar(100) DEFAULT NULL,
+  FOREIGN KEY (`order_id`) REFERENCES `order`(`order_id`)
+  FOREIGN KEY (`product_id`) REFERENCES `product`(`product_id`)
+);
+
+CREATE TABLE `product_warehouse` (
+  `warehouse_id` int(20) DEFAULT NULL,
+  `product_id` int(20) DEFAULT NULL,
+  FOREIGN KEY (`product_id`) REFERENCES `product`(`product_id`),
+  FOREIGN KEY (`warehouse_id`) REFERENCES `warehouse`(`warehouse_id`)
+);
