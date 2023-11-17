d1497c23962989f62e087f75f12a3183ac62a82c
Jaytonn-H02 <122837315+Jaytonn-H02@users.noreply.github.com>
2023-11-17 18:04:54 -0500

    Update view_customers.php
    still not done

--- hw8/view_customers.php
+++ hw8/view_customers.php
@@ -8,11 +8,18 @@
 <?php
 li><a href="http://localhost/3220/hw08/manager.html">Home Page</a>
 
-
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+or die ('Could not connect to the database server' . mysqli_connect_error());
 $stmt = $mysqli->prepare("SELECT last_name, first_name, address, phone, district FROM customer inner JOIN address ON customer.address_id = address.address_id");
 $stmt->bind_param("si", $_POST['name'], $_POST['age']);
 $stmt->execute();
 $stmt->bind_result($lname, $fname, $address, $phone, $district);
+
+while ($stmt->fetch()) {
+printf("%s, %s, %s, %s\n",
+$lname, $fname, $address, $phone, $district);
+}
+
 $stmt->close();
 
 
