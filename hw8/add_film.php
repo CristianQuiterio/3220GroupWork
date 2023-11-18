6beb246749c39c30bbe0a37acbbeb4c5e63b1822
CristianQuiterio <47873089+CristianQuiterio@users.noreply.github.com>
2023-11-17 20:37:12 -0500

    Update add_film.php

--- hw8/add_film.php
+++ hw8/add_film.php
@@ -7,6 +7,8 @@
 <body>
 <h1></h1>
 <?php
+try{
+
 $mysqli = new mysqli("localhost", "root", "", "sakila");
 if($mysqli->connect_error) {
 exit('Error connecting to database'); //Should be a message a typical user could understand in production
@@ -17,12 +19,24 @@ $mysqli->set_charset("utf8mb4");
 $stmt = $mysqli->prepare("INSERT INTO sakila.film (title,description,release_year,language_id,rental_duration,rental_rate,length,replacement_cost,rating,special_features) VALUES (?,?,?,?,?,?,?,?,?,?)");
 $stmt->bind_param('ssisididss', $_POST['title'], $_POST['description'], $_POST['releaseYear'], $_POST['langID'], $_POST['rentDuration'], $_POST['rentRate'], $_POST['length'], $_POST['repCost'], $_POST['rating'], $_POST['special_features']);
 $stmt->execute();
-$stmt->close();
-;
 
+if($stmt->affected_rows === 0) exit('No rows updated');
+$stmt->close();
 $mysqli->close();
 
-print ("Success!!");
+print ("Success!!<br>");
+print("<form action = ".'manager.html'." method = ".'post'." >
+<input type = ".'submit'." value = ".'Add Film'." >
+</form>");
+} catch(Exception $e) {
+print ("An Error Has Occured. Did not add to the DB!!<br>");
+print("<form action = ".'manager.html'." method = ".'post'." >
+<input type = ".'submit'." value = ".'Add Film'." >
+</form>");
+}
+
+
+
 ?>
 </body>
 </html>
