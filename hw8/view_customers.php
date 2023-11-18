3ea220abe2ef2f76245b9bbacc4a39243935ba2d
Jaytonn-H02 <122837315+Jaytonn-H02@users.noreply.github.com>
2023-11-17 20:40:38 -0500

    Update view_customers.php

--- hw8/view_customers.php
+++ hw8/view_customers.php
@@ -11,7 +11,6 @@
 $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
 or die ('Could not connect to the database server' . mysqli_connect_error());
 $stmt = $mysqli->prepare("SELECT last_name, first_name, address, phone, district FROM customer inner JOIN address ON customer.address_id = address.address_id");
-$stmt->bind_param("si", $_POST['name'], $_POST['age']);
 $stmt->execute();
 $stmt->bind_result($lname, $fname, $address, $phone, $district);
 
