da299e8c41d6de76edbc13a35377f9fe3819d7a8
CristianQuiterio <47873089+CristianQuiterio@users.noreply.github.com>
2023-11-17 22:38:43 -0500

    added report

--- hw8/view_customers.php
+++ hw8/view_customers.php
@@ -17,10 +17,9 @@ exit('Error connecting to database'); //Should be a message a typical user could
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $mysqli->set_charset("utf8mb4");
 
-$stmt = $mysqli->prepare("SELECT last_name, first_name, address, phone, district, title FROM customer inner JOIN address ON customer.address_id = address.address_id inner JOIN rental on customer.customer_id = rental.customer.id inner JOIN inventory on rental.inventory_id = inventory.inventory_id inner JOIN film on inventory.film_id =film.film_id ORDER BY last_name");
+$stmt = $mysqli->prepare("SELECT last_name, first_name, address, phone, district, title FROM customer inner JOIN address ON customer.address_id = address.address_id inner JOIN rental on customer.customer_id = rental.customer_id inner JOIN inventory on rental.inventory_id = inventory.inventory_id inner JOIN film on inventory.film_id =film.film_id ORDER BY last_name");
 $stmt->execute();
 $stmt->bind_result($lname, $fname, $address, $phone, $district, $title);
-
 
 //make the table header
 print "<table>";
@@ -35,8 +34,7 @@ while ($stmt->fetch()) {
   print "<td>".$address."</td>";
   print "<td>".$phone."</td>";
   print "<td>".$district."</td>";
-  print "<td>".$title."</td>";
-  print "</tr>";
+  print "<td>".$title."</tr>";
 }
 print "</table>";
 
@@ -44,7 +42,8 @@ $stmt->close();
 
 
 }catch(Exception $e) {
-print ("An Error Has Occured. Did not find the DB!!<br>");
+//print ("An Error Has Occured. Did not find the DB!!<br>");
+print ($e);
 }
 ?>
 </body>
