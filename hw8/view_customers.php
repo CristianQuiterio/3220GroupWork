6371995fce50178dd759c15861eb44bcba58579d
Jaytonn-H02 <122837315+Jaytonn-H02@users.noreply.github.com>
2023-11-17 20:41:48 -0500

    Update view_customers.php

--- hw8/view_customers.php
+++ hw8/view_customers.php
@@ -8,8 +8,15 @@
 <?php
 <li><a href="http://localhost/3220/hw08/manager.html">Home Page</a>
 
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-or die ('Could not connect to the database server' . mysqli_connect_error());
+try{
+
+$mysqli = new mysqli("localhost", "root", "", "sakila");
+if($mysqli->connect_error) {
+exit('Error connecting to database'); //Should be a message a typical user could understand in production
+}
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$mysqli->set_charset("utf8mb4");
+
 $stmt = $mysqli->prepare("SELECT last_name, first_name, address, phone, district FROM customer inner JOIN address ON customer.address_id = address.address_id");
 $stmt->execute();
 $stmt->bind_result($lname, $fname, $address, $phone, $district);
